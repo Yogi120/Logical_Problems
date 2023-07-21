ca24661899ba8fde1220541bcf191b4b4f7fecf7
Yogi120 <me.yogesh.pal@gmail.com>
2023-07-21 09:08:34 +0530

    Add two Number

---
+++ LogicalProblems/Basics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public  class Basics
+    {
+        public int Product(int a, int b, int c)
+        {
+            return (a + b) * c;
+        }
+    }
+}
---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Basic");
+            Console.WriteLine("2. Conditional Statements");
+            Console.WriteLine("3. Loops");
+            Console.WriteLine("4. Strings");
+
+            Console.Write("Choose Option: ");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:
+
+                    Basics basic = new Basics();
+
+                    Console.WriteLine("1. Add two numbers");
+                    Console.WriteLine("2. Celsius to Fahrenheit ");
+                    Console.WriteLine("3. Elementary operations");
+                    Console.WriteLine("4. Is result the same ");
+                    Console.WriteLine("5. Modulo operations");
+                    Console.WriteLine("6. The cube of");
+                    Console.WriteLine("7. Swap two numbers ");
+
+                    Console.Write("Choode Option: ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            
+                            Console.WriteLine(basic.Product(5, 6, 8));
+                            break;
+
+                    }
+                    break;
+            }
+
+
+        }
+    }
+}
